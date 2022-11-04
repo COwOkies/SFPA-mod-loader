a1a3f3866f90eb703f5a819571e7735ac49b354b
COwOkies_ <54218300+COwOkies@users.noreply.github.com>
2022-11-04 21:10:18 +0100

    Update SFPA mod-loader.py

--- SFPA mod-loader.py
+++ SFPA mod-loader.py
@@ -5,26 +5,21 @@ import shutil
 print("log window\n-----------\n")
     
 def button_pressed(button):
-    replaceMod(button.cget("text"))
+    if button.cget("bg") == '#ffea00': ext=".swp"
+    else: ext=".swf"
+    replaceMod(button.cget("text"),ext)
     
-def replaceMod(modname):
-    modname = str(modname)+".swf"
-    if (os.path.isfile(r'game\SFPA.swf')):
-        os.remove(r'game\SFPA.swf')
-        print("'game\SFPA.swf' deleted")
-    else: print("'game\SFPA.swf' doesn't exist")
+def replaceMod(modname,ext):
 
-    
-    shutil.copy("files\\"+modname,"game")
-    print("'files\\"+modname+"' copied to 'game' folder")
-    
-    os.rename(r'game\\'+modname,r'game\SFPA.swf')
-    print("'"+modname+"' renamed to SFPA.swf")
+    modname=str(modname)+ext
+    shutil.copy("files\\"+modname,"game\SFPA.swf")
+    print("Replacing SFPA.swf with "+modname)
     launchGame()
 
 def launchGame():
-    os.system("TASKKILL /F /IM SFPA.exe")
-    print("Launching game...")
+    print("Killing SFPA.exe if running :\n")
+    os.system('tasklist | find /i "SFPA.exe" && taskkill /im SFPA.exe /F || echo process "SFPA.exe" not running.')
+    print("\nLaunching game...")
     os.startfile(r'game\SFPA.exe')
 
 window = tk.Tk()
@@ -32,21 +27,23 @@ window.title("SFPA mod-loader")
 window.configure(bg='#ffc800')
 window.iconbitmap("fancy.ico")
 
+
 mods = os.listdir(r'files')
 text_list = []
 
-tk.Label(text="Mod List",bg='#ffc800',font=("Verdana", 15,"bold")).pack()
+tk.Label(window, text="Mod List",bg='#ffc800',font=("Verdana", 15,"bold")).pack(side="top",fill="x")
 
 
 for mod in mods:
-    if str(mod[-4:]) == ".swf":
-        mod = str(mod)[:-4]
-        button = tk.Button(window, text=mod,bg='#ffaa00',font=("Verdana", 13))
+    if str(mod[-4:]) == ".swf" or str(mod[-4:]) == ".swp":
+        modname = str(mod)[:-4]
+        button = tk.Button(window, text=modname,bg='#ffaa00',font=("Verdana", 13))
+        if str(mod[-4:]) == ".swp": button.config(bg='#ffea00')
         button.config(command=lambda button=button: button_pressed(button))
-        button.pack(side="top", fill="both", expand=True)
+        button.pack(side="top", fill="x", expand=True)
         text_list.append(button)
 
-infolabel = tk.Label(text="Please select a mod",bg='#ffc800',font=("Verdana", 11))
+infolabel = tk.Label(window, text="Please select a mod",bg='#ffc800',font=("Verdana", 11))
 
 infolabel.pack()
 window.mainloop()
